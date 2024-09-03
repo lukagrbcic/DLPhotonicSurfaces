45dc088b34c22f9e28dda4e7fca79a3eafaf54f5
lukagrbcic <luka.grbcic@gmail.com>
2024-09-03 11:28:57 -0700

    TNN code ready

--- TNN/tnn.py
+++ TNN/tnn.py
@@ -5,8 +5,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 from torch.utils.data import DataLoader, TensorDataset
 import joblib
+from sklearn.model_selection import train_test_split
 
-import inverse_forward_architecture as ifa
+plt.rcParams.update({
+    "text.usetex": True,
+    'font.family': 'sans-serif',
+    'text.latex.preamble': r'\usepackage{sfmath} \sffamily \usepackage{upgreek}',
+    "font.size": 18,
+})
 
 
 
@@ -21,11 +27,11 @@ class tandem_model:
         
         self.train_data = train_data #tuple (inputs, outputs)
         self.test_data = test_data #tuple (inputs, outputs)
-        self.forward_architecture = forward_architecture #generator object
-        self.inverse_architecture = inverse_architecture #discriminator object
+        self.forward_architecture = forward_architecture #forward DNN architecture
+        self.inverse_architecture = inverse_architecture #inverse DNN architecutre
         self.epochs = epochs 
         self.batch_size = batch_size
-        self.forward_model = forward_model #tuple (ml_model, pca_model)
+        self.forward_model = forward_model #tuple (ml_model, pca_model) #load forward DNN here (include minmax scaler)
         self.verbose = verbose
         self.device = device
         self.rmse_loss = rmse_loss
@@ -49,6 +55,33 @@ class tandem_model:
     def rmse(outputs, targets):
         return torch.sqrt(torch.mean((outputs - targets) ** 2))
 
+
+    @staticmethod 
+    def _normalize(parameters):
+        
+        lspeed_min, spacing_min, power_min = 0.2, 10, 15 
+        lspeed_max, spacing_max, power_max = 1.3, 700, 28
+        
+        lspeed_norm = (parameters[:,0] - lspeed_min)/(lspeed_max - lspeed_min)
+        spacing_norm = (parameters[:, 1] - spacing_min)/(spacing_max - spacing_min)
+        power_norm = (parameters[:, 2] - power_min)/(power_max - power_min)
+            
+        return lspeed_norm, spacing_norm, power_norm
+
+
+    def get_nepd(self, preds, test_data):
+        
+        lspeed_norm_true, spacing_norm_true, power_norm_true = self._normalize(test_data)
+        lspeed_norm_pred, spacing_norm_pred, power_norm_pred = self._normalize(preds)
+    
+        normalized_true = np.hstack((lspeed_norm_true, spacing_norm_true, power_norm_true))
+        normalized_pred = np.hstack((lspeed_norm_pred, spacing_norm_pred, power_norm_pred))
+        
+        nepd_value = (1/np.sqrt(3)) * np.sqrt((lspeed_norm_true - lspeed_norm_pred)**2 +\
+                                      (spacing_norm_true - spacing_norm_pred)**2 +\
+                                      (power_norm_true - power_norm_pred)**2)
+            
+        return nepd_value
     
     def forward_prediction(self, prediction):
                  
@@ -69,19 +102,28 @@ class tandem_model:
         print ('TRAINING MODE')
         print ('Using:', self.device)
         
-        print (len(self.train_data[0]))
+        X_train, X_val, y_train, y_val = train_test_split(self.train_data[0], 
+                                                          self.train_data[1],
+                                                          test_size=0.1, 
+                                                          shuffle=False, 
+                                                          random_state=23)
+                        
 
-                
-        dataloader = self.get_torch_dataloader(self.train_data)
+        train_loader = self.get_torch_dataloader((X_train, y_train))
+        val_loader = self.get_torch_dataloader((X_val, y_val))
+
 
         forward = self.forward_architecture
-        model = self.inverse_architecture
+        forward.load_state_dict(torch.load(self.forward_model[0]))
+        forward.eval()
+        
+        inverse = self.inverse_architecture
 
         def criterion(outputs, targets):
             return torch.sqrt(torch.mean((outputs - targets) ** 2))
 
 
-        optimizer = optim.Adam(forward_.parameters(), lr=0.0005)
+        optimizer = optim.Adam(inverse.parameters(), lr=0.0004) #0.0002
         early_stopping_patience = 5
         best_loss = float('inf')
         epochs_no_improve = 0
@@ -89,41 +131,44 @@ class tandem_model:
         train_losses = []
         val_losses = []
 
-        # Training the model
         num_epochs = self.epochs
         for epoch in range(num_epochs):
-            model.train()
+            inverse.train()
             epoch_train_loss = 0
             for inputs, targets in train_loader:
                 optimizer.zero_grad()
-                outputs = model(inputs)
-                loss = criterion(outputs, targets)
+                outputs = inverse(inputs)
+
+                emissivity_output = forward(outputs)
+           
+                loss = criterion(emissivity_output, inputs)
                 loss.backward()
                 optimizer.step()
-                epoch_train_loss += loss.item()  # Accumulate training loss
-
-            avg_train_loss = epoch_train_loss / len(train_loader)  # Calculate average training loss
+                epoch_train_loss += loss.item()  
+        
+            avg_train_loss = epoch_train_loss / len(train_loader)
             train_losses.append(avg_train_loss)
             
-            # Validation loss
-            model.eval()
+            inverse.eval()
             with torch.no_grad():
                 total_val_loss = 0
                 for inputs, targets in val_loader:
-                    outputs = model(inputs)
-                    loss = criterion(outputs, targets)            
-                    total_val_loss += loss.item()  # Accumulate validation loss
+                    outputs = inverse(inputs)
+                    emissivity_output = forward(outputs)
+        
+                    loss = criterion(emissivity_output, inputs)       
+        
+                    total_val_loss += loss.item()  
                     
-            avg_val_loss = total_val_loss / len(val_loader)  # Calculate average validation loss
-            val_losses.append(avg_val_loss)
-
+                avg_val_loss = total_val_loss / len(val_loader) 
+                val_losses.append(avg_val_loss)
+            
             print(f'Epoch {epoch+1}/{num_epochs}, Training Loss: {avg_train_loss}, Validation Loss: {avg_val_loss}')
             
-            # Early stopping logic
             if avg_val_loss < best_loss:
                 best_loss = avg_val_loss
                 epochs_no_improve = 0
-                best_model_wts = model.state_dict().copy()
+                best_model_wts = inverse.state_dict().copy()
             else:
                 epochs_no_improve += 1
             
@@ -131,60 +176,112 @@ class tandem_model:
                 print(f'Early stopping at epoch {epoch+1}')
                 break
 
-            # print(f'Epoch {epoch+1}/{num_epochs}, Loss: {loss.item()}')
+
             
-        torch.save(model.state_dict(), 'forward_model.pth')
-        
-        plt.figure(figsize=(10, 5))
-        plt.plot(rmse_loss_plot, label='RMSE Loss')
+        torch.save(inverse.state_dict(), 'inverseModel/inverse_model.pth')
+
+        plt.figure(figsize=(6, 5))
+        plt.plot(np.array(train_losses)*100, label='Training Loss')
+        plt.plot(np.array(val_losses)*100, label='Validation Loss')
         plt.xlabel('Epoch')
-        plt.ylabel('Loss')
+        plt.ylabel('RMSE Loss (\%)')
+        plt.ylim(0, 10)
         plt.legend()
-        plt.savefig(f'generator_{self.material}/rmse_loss_{len(self.train_data[0])}.png', dpi=400)
-        plt.show()
+        ax = plt.gca()
+               
+        for axis in ['top', 'bottom', 'left', 'right']:
+            ax.spines[axis].set_linewidth(2)
+            
+        plt.savefig('TNN_loss.pdf', bbox_inches='tight', format='pdf', dpi=500)
+
         
     
-    def test(self, size):
+    def test(self):
         
+        def criterion(outputs, targets):
+            return torch.sqrt(torch.mean((outputs - targets) ** 2))
         
         print ('TESTING MODE')
         print ('Using:', self.device)
         
-        generator = self.generator
-        generator.load_state_dict(torch.load(f'generator_{self.material}/generator_{size}.pth'))
+        
+        forward = self.forward_architecture
+        forward.load_state_dict(torch.load(self.forward_model[0]))
+        forward.eval()
+        
+        inverse = self.inverse_architecture
+        inverse.load_state_dict(torch.load('./inverseModel/inverse_model.pth'))
+        inverse.eval()
+        
+        test_loader = self.get_torch_dataloader(self.test_data, inference=True)
+        pca_model = self.forward_model[1]
 
-        generator.eval()
-        
-        noise_dim = self.noise_dim
-        dataloader = self.get_torch_dataloader(self.test_data, inference=True)
-        
-        rmse_values = []
-        
-        for inputs, targets in dataloader:
+        predictions = []
+        laser_params = []
+        rmse_loss = []
+        with torch.no_grad():
 
-            noise_vector = torch.randn(1, noise_dim, device=self.device)
-            predictions = generator(noise_vector, inputs)
-
-            rmse_loss = self.rmse(self.forward_prediction(predictions), inputs)
-            rmse_values.append(rmse_loss.item())
+            for inputs, targets in test_loader:
+                
+                
+                outputs = inverse(inputs)
+                laser_params.append(outputs.detach().cpu().numpy())                
+                emissivity_output = forward(outputs)
         
+                predictions.append(emissivity_output.detach().cpu().numpy())
+                
+                rmse = criterion(emissivity_output, inputs)
+                rmse_loss.append(rmse.cpu().numpy())
+            
+        emissivity_predictions = np.concatenate(predictions)
+        laser_params_predictions = np.concatenate(laser_params)
+        rmse_loss = [i.item() for i in rmse_loss]
+        return emissivity_predictions, laser_params_predictions, rmse_loss
+        
+    def post_process(self, emissivity_predictions, laser_params_predictions, rmse):
+        
+        preds = self.forward_model[1].inverse_transform(laser_params_predictions)
+        nepd = self.get_nepd(preds, self.test_data[1])
+        rmse = np.array(rmse)*100
+       
+        plt.figure(figsize=(6,5))
+        plt.scatter(np.array(nepd), rmse, color='lightblue', marker='o', alpha=0.9)
+       
+       
+        max_nepd = np.max(nepd)
+        avg_nepd = np.mean(nepd)
+        max_rmse = np.max(rmse)
+        avg_rmse = np.mean(rmse)
+       
+        plt.axvline(max_nepd, color='grey', linestyle='--', linewidth=1)
+        plt.axvline(avg_nepd, color='grey', linestyle='--', linewidth=1)
+       
+        plt.axhline(max_rmse, color='grey', linestyle='--', linewidth=1)
+        plt.axhline(avg_rmse, color='grey', linestyle='--', linewidth=1)
+       
+        plt.text(max_nepd, plt.ylim()[1]*0.35, f'Max NEPD {max_nepd:.2f} \%', horizontalalignment='right', rotation=90)
+        plt.text(avg_nepd+0.02, plt.ylim()[1]*0.35, f'Avg. NEPD {avg_nepd:.2f} \%', horizontalalignment='left', rotation=90)
+       
      
-        std_rmse = np.std(rmse_values)
-        max_rmse = np.max(rmse_values)
-        mean_rmse = np.mean(rmse_values)
+        right_edge = plt.xlim()[1]
+        padding = (right_edge - plt.xlim()[0]) * 0.01  # 2% padding from the right edge
+        plt.text(right_edge - padding, max_rmse, f'Max RMSE {max_rmse:.2f} \%', verticalalignment='bottom', horizontalalignment='right')
+        plt.text(right_edge - padding, avg_rmse-0.1, f'Avg. RMSE {avg_rmse:.2f} \%', verticalalignment='top', horizontalalignment='right')
+       
+       
+        plt.xlabel('Design novelty (NEPD)')
+        plt.ylabel('RMSE (\%)')
+        plt.ylim(0, 10)
+        plt.xlim(0, 1)
+       
+        ax = plt.gca()
+       
+        for axis in ['top', 'bottom', 'left', 'right']:
+            ax.spines[axis].set_linewidth(2)
+        plt.savefig('rmse_vs_nepd_TNN_inconel.pdf', bbox_inches='tight', format='pdf', dpi=500)
+        
 
         
-        print ('Mean RMSE:', mean_rmse)
-        print ('Std RMSE:', std_rmse)
-        print ('Max RMSE:', max_rmse)
-        
-        plt.figure()
-        plt.xlabel('Value')
-        plt.ylabel('RMSE')
-        plt.savefig(f'generator_{self.material}/hist_rmse_loss_{size}_testing_{len(self.test_data[0])}.png', dpi=400)
-        
-        return mean_rmse, max_rmse, std_rmse, rmse_values
-    
 
         
         
