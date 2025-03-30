684f036de5f344be79da092277e65cd5674b020f
vpatro <vpatro@lbl.gov>
2025-03-29 21:51:04 -0700

    all bugs fixed and all experimnts working

--- TNN/main.py
+++ TNN/main.py
@@ -185,11 +185,11 @@ def main():
         result_dir = f'{result_dir}/{args.num_inverse_layers_to_transfer}_layers_transferred' if args.inverse_DNN_hot_start_dataset != 'from_scratch' else result_dir
         os.makedirs(result_dir, exist_ok=True)
         forward_hot_start = 'from_scratch'
-        forward_hot_start = args.forward_DNN_hot_start_dataset + 'hot_start' if args.forward_DNN_hot_start else forward_hot_start
+        forward_hot_start = args.forward_DNN_hot_start_dataset + '_hot_start' if args.forward_DNN_hot_start else forward_hot_start
         inverse_hot_start = 'hot_start_' + args.inverse_DNN_hot_start_dataset if args.configuration == 'transfer_learning' else 'from_scratch'
         forward_transfer_descriptor = ''
-        forward_transfer_descriptor = f'{args.num_forward_layers_transferred}_layers_' if args.forward_DNN_hot_start else forward_transfer_descriptor
-        outfile = f'{result_dir}/{args.configuration}_{args.dataset_name}_dataset_forwardDNN_{forward_transfer_descriptor}_{forward_hot_start}_inverseDNN_{inverse_hot_start}.json'
+        forward_transfer_descriptor = f'_{args.num_forward_layers_transferred}_layers' if args.forward_DNN_hot_start else forward_transfer_descriptor
+        outfile = f'{result_dir}/{args.configuration}_{args.dataset_name}_dataset_forwardDNN{forward_transfer_descriptor}_{forward_hot_start}_inverseDNN_{inverse_hot_start}.json'
 
         obj = {'mean train loss': mean_train_loss,
                'mean val loss': mean_val_loss,
